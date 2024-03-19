6b454b86efdcb68f971756e319a7ce8ac87fd9f3
mamad010 <163658141+mamad010@users.noreply.github.com>
2024-03-19 14:38:13 +0330

    Create Kabir_Cyber.py

---
+++ Kabir_Cyber.py
@@ -0,0 +1,64 @@
+from colorama import Fore,init
+init()
+print(Fore.BLUE+"     FILTERING RUBIKA MAMAD KABIR ")
+
+
+
+print(Fore.GREEN+"""
+
+⠀#####                                                             #     #   ##    ####  #####  #         #    #   ##    ####  #    # #        #  #  #      #    # #         #    #  #  #  #    # #   #  #       #    #  ####  #    # #         ###### #    # #      ####   #       ######      # #####  #         #    # ###### #      #  #   #     # #    # #    # #      #         #    # #    # #    # #   #   #####  #    #  ####  #      # ####### #    # #    #  ####  #    #
+
+""")
+from colorama import Fore,init
+init()
+print(Fore.BLUE+"     FILTERING RUBIKA MAMAD KABIR")
+
+
+
+print(Fore.GREEN+"""
+
+███████████████████████████
+███████▀▀▀░░░░░░░▀▀▀███████
+████▀░░░░░░░░░░░░░░░░░▀████
+███│░░░░░░░░░░░░░░░░░░░│███
+██▌│░░░░░░░░░░░░░░░░░░░│▐██
+██░└┐░░░░░░░░░░░░░░░░░┌┘░██
+██░░└┐░░░░░░░░░░░░░░░┌┘░░██
+██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
+██▌░│██████▌░░░▐██████│░▐██
+███░│▐███▀▀░░▄░░▀▀███▌│░███
+██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
+██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
+████▄─┘██▌░░░░░░░▐██└─▄████
+█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
+████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
+█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
+███████▄░░░░░░░░░░░▄███████
+██████████▄▄▄▄▄▄▄██████████
+███████████████████████████
+""")
+from colorama import Fore,init
+init()
+print(Fore.RED+"       HACK RUbIKA")
+print('')
+print('››››››   @Cyber_Master_Cyber‹‹‹‹‹‹')
+print('')
+print('››››››  @caspi_hack‹‹‹‹‹‹')
+print('')
+from colorama import Fore,init
+init()
+print(Fore.BLUE+"     FILTERING  ")
+from colorama import Fore,init
+init()
+print(Fore.RED+"")
+name =input('  filtering yfftt14 :›››1 ' )
+print('')
+print('https://该用户邀请他人加入.ISIS.怖组织。该用户对社会有.请阻止该用户')
+print('')
+name =input('  HACK yfftt14 :›››2 ' )
+print('')
+print('http://dxprit-fil-rubika-sxs-sex-hacker-supportbot.phpnet.us/(*https://dscript.com/yftt12k/yftt-filter-rubika.hackers72748')
+print('')
+name =input('  HACK fil ter : ›››3 ' )
+print('')
+print('http://dds6qkxpwdeubwucdiaord2xgbbeyds25rbsgr73tbfpqpt4a6vjwsyd.onion/http://cathug2kyi4ilneggumrenayhuhsvrgn6qv2y47bgeet42iivkpynqad.onion/*)(*https://www.uplooder.net/img/image/45/7aa3c4d6a5d5b09a0b4942b08effd0b6/IMG-20230517-232423-114.jpg)(*http://4.0.1.4/f.i.d/gl/4.5.3//5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3.4.0.1.4.4.5.3.5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3')
